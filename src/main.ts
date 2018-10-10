c0b5058fec1ab4fae8b6c034d282d9c1daa77c72
lukaskurz <lukaskurz5@gmail.com>
2018-10-10 10:48:26 +0200

    changed message handling to be more scalable

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import MessageManager from './message-handler';
 const mqtt = new MqttManager(Config);
 let messages: Subject<MqttMessage>;
 let messageManager = new MessageManager(Config)
+initMessageManager();
 
 mqtt.connect()
     .then(() => {
@@ -23,3 +24,54 @@ mqtt.connect()
     .catch(() => {
         console.log("Error in MQTT process");
     });
+
+function initMessageManager() {
+    const statusHandler = (message: MqttMessage, scope: any) => {
+        scope = scope as { lastStatus: number };
+        const timeAgo = (new Date().getTime() - scope.lastStatus);
+        if (timeAgo > Config.get("api.endpoints.carbyStatus.interval")) {
+            scope.lastStatus = new Date().getTime();
+            sendUpdate(Config.get("api.endpoints.carbyStatus.url"), `Current CO² level: ${message.data.value}ppm`);
+        }
+    }
+
+    const statusScope = {
+        lastStatus: new Date().getTime()
+    };
+
+    messageManager.addHandler(statusHandler, statusScope);
+
+    const generalHandler = (message: MqttMessage, scope: any) => {
+        scope = scope as { lastAlert: number, alertReached: boolean };
+        if (message.data.value > Config.get("api.endpoints.general.upperlimit")) {
+            const timeAgo = (new Date().getTime() - scope.lastAlert);
+            if (timeAgo > Config.get("api.endpoints.general.interval")) {
+                scope.lastAlert = new Date().getTime();
+                scope.alertReached = true;
+                sendUpdate(Config.get("api.endpoints.general.url"), `The CO² levels have reached an unbearable high of ${message.data.value}ppm ! :anguished:`);
+            }
+        }
+
+        if (scope.alertReached == true) {
+            if (message.data.value < Config.get("api.endpoints.general.lowerlimit")) {
+                scope.alertReached = false;
+                sendUpdate(Config.get("api.endpoints.general.url"), `The CO² levels have reached an acceptable low of ${message.data.value}ppm :sunglasses:`);
+            }
+        }
+    }
+
+    const generalScope = {
+        lastAlert: new Date().getTime(),
+        alertReached: false
+    };
+
+
+    messageManager.addHandler(generalHandler, generalScope);
+
+}
+
+function sendUpdate(url: string, message: string) {
+    Axios.post(url, {
+        text: message,
+    })
+}
--- src/message-handler.ts
+++ src/message-handler.ts
@@ -1,42 +1,28 @@
 import { IConfig } from "config";
-import Axios from 'axios';
 import { MqttMessage } from "./mqtt-client";
 
 export default class MessageManager {
-    lastStatus: number = new Date(1970,12).getTime();
-    lastAlert: number = new Date(1970,12).getTime();
+    lastStatus: number = new Date(1970, 12).getTime();
+    lastAlert: number = new Date(1970, 12).getTime();
     alertReached = false;
+    private indexCounter = 0;
 
-    constructor(public config: IConfig) { }
+    handlers: { id: number, handler: ((message: MqttMessage, scope: Object) => void), scope: Object }[];
 
-    sendUpdate(url: string, message: string) {
-        Axios.post(url, {
-            text: message,
-        })
+    constructor(public config: IConfig) {
+        this.handlers = [];
     }
 
     handle(message: MqttMessage) {
-        const timeAgo = (new Date().getTime() - this.lastStatus);
-        if (timeAgo > this.config.get("api.endpoints.carbyStatus.interval")) {
-            this.lastStatus = new Date().getTime();
-            this.sendUpdate(this.config.get("api.endpoints.carbyStatus.url"), `Current CO² level: ${message.data.value}ppm`);
+        let index = this.handlers.length;
+        while (index--) {
+            const currentHandler = this.handlers[index];
+            currentHandler.handler(message, currentHandler.scope);
         }
+    }
 
-        if (message.data.value > this.config.get("api.endpoints.general.upperlimit")) {
-            const timeAgo = (new Date().getTime() - this.lastAlert);
-            if (timeAgo > this.config.get("api.endpoints.general.interval")) {
-                this.lastAlert = new Date().getTime();
-                this.alertReached = true;
-                this.sendUpdate(this.config.get("api.endpoints.general.url"), `The CO² levels have reached an unbearable high of ${message.data.value}ppm ! :anguished:`);
-            }
-        }
-
-        if(this.alertReached == true){
-            if(message.data.value < this.config.get("api.endpoints.general.lowerlimit")){
-                this.alertReached = false;
-                this.sendUpdate(this.config.get("api.endpoints.general.url"), `The CO² levels have reached an acceptable low of ${message.data.value}ppm :sunglasses:`);
-            }
-        }
+    addHandler(handler: (message: MqttMessage, scope: {}) => void, localScope: any) {
+        this.handlers.push({ id: this.indexCounter++, handler: handler, scope: localScope });
     }
 }
 
